370213717fd01c4e2b74c364c9f9bbe0938abdb8
FhadiR04 <fikihadi0411@gmail.com>
2023-05-12 15:23:39 +0700

    tugaswpsaya

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,13 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h1>";
+        echo"Nama saya Fiki Hadi Rulianto
+            Saya tinggal di daerah Priok 
+            olah raga yang saya sukai adalah
+            Billiard";
+    }
+}
+# pustaka-booking
---
+++ application/views/v_about.php
@@ -0,0 +1,28 @@
+<section>
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4>
+    <ul type="disc">
+        <li>Nama Depan : Fiki Hadi </li>
+        <li>Nama Belakang : Rulianto</li>
+</ul>
+<br>
+<h4>Alamat</h4>
+<ul type="none">
+    <li> Jl. PAPANGGO II NO.44 </li>
+</ul>
+
+<h4>Tempat Lahir</h4>
+<ul type="none">
+    <li>Jakarta Utara</li>
+</ul> 
+
+<h4>Tanggal Lahir</h4>
+<ul type="none">
+    <li>04 November 2001</li>
+</ul>
+ <h4>Hobi</h4>
+ <ul type="square">
+    <li>Mencintai Dalam Diam</li>
+    <li>Melukis Wajahmu</li>
+</ul>
+</section>
